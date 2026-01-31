5b87e5f20c2945cfe73c623d813e35641c03231c
Leemo-ui <merkitjohn@gmail.com>
2026-02-01 00:17:06 +0300

    Sync: push all changes

--- zanaco-loan/vendor/composer/installed.php
+++ zanaco-loan/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '578a20d93e58b95955793baa2a3efc0e82b94e51',
+        'reference' => 'b93c3f6c2679eb925b793e9062608651fc699894',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '578a20d93e58b95955793baa2a3efc0e82b94e51',
+            'reference' => 'b93c3f6c2679eb925b793e9062608651fc699894',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
